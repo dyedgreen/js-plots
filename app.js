e9bda72a5c2b11f1100018438f9544b841dc9a8e
Tilman Roeder <dyed.green.info@gmail.com>
2019-03-16 12:01:13 +0000

    Plots now work, need to style buttons

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ class Plot extends HTMLElement {
     this.appendChild(this.cnv);
     window.onresize = Plot.shareEvent(() => window.requestAnimationFrame(() => this.resize()), window.onresize);
     window.requestAnimationFrame(() => this.resize());
-    this.cnv.onmousemove = (e) => window.requestAnimationFrame(() => this.onmousemove(e));
+    this.cnv.onmousemove = (e) => window.requestAnimationFrame(() => this.onpointermove(e));
     this.cnv.onmouseenter = () => this.onhover(true);
     this.cnv.onmouseleave = () => this.onhover(false);
     // Canvas and style
@@ -67,14 +67,10 @@ class Plot extends HTMLElement {
     }
   }
 
-  setFromTo(xFrom, xTo, animate) {
+  setFromTo(from, to, animate) {
     // Select from and to
-    for (this.from = 0; this.from < this.x.length-1; this.from ++) {
-      if (this.x[this.from+1] > xFrom) break;
-    }
-    for (this.to = this.x.length-1; this.to > 0; this.to --) {
-      if (this.x[this.to-1] < xTo) break;
-    }
+    this.from = Math.max(0, from);
+    this.to = Math.min(this.x.length, to);
     if (animate) {
       this.animate();
     } else {
@@ -91,7 +87,6 @@ class Plot extends HTMLElement {
     this.cnv.style.width = `${w}px`;
     this.cnv.style.height = `${h}px`;
     this.style.height = `${h}px`;
-    // FIXME: REDRAW canvas
     this.show();
   }
 
@@ -135,7 +130,8 @@ class Plot extends HTMLElement {
     window.requestAnimationFrame(closure);
   }
 
-  onmousemove(event) {
+  onpointermove(event) {
+    if (!this.axis) return;
     // Determine highlight point
     this.hoverIndex = (event.clientX - this.cnv.getBoundingClientRect().left) / this.cnv.offsetWidth;
     this.hoverIndex = Math.floor(this.hoverIndex * (this.to-this.from) + this.from);
@@ -143,6 +139,7 @@ class Plot extends HTMLElement {
   }
 
   onhover(hovered) {
+    if (!this.axis) return;
     this.hovered = !!hovered;
     this.show();
   }
@@ -191,6 +188,7 @@ class Plot extends HTMLElement {
     const offset = this.fontHeight * Plot.pixelRatio;
     const fontOffset = (this.fontHeight - this.fontSize) / 2 * Plot.pixelRatio;
     let   ySpacing = (yMax - yMin) / this.yTicks; // in y-units
+    let   yTicks = this.yTicks;
     const xSpacing = (xMax - xMin) / this.xTicks; // in x-units
     const yScale = (this.cnv.height-2*offset) / (yMax - yMin);
     const xScale = (this.cnv.width) / (xMax - xMin);
@@ -203,7 +201,10 @@ class Plot extends HTMLElement {
       if (yMax > 100000) { s = 10000; }
       else if (yMax > 10000) { s = 1000; }
       else if (yMax > 1000) { s = 100; }
+      else if (yMax > 100) { s = 10; }
+      else if (yMax > 10) { s = 0.1; }
       ySpacing = Math.floor(ySpacing / s) * s;
+      yTicks = Math.floor(yMax / ySpacing);
     }
 
     // Render background
@@ -217,7 +218,7 @@ class Plot extends HTMLElement {
       this.ctx.lineWidth = Plot.pixelRatio;
 
       this.ctx.beginPath();
-      for (let i = 0; i <= this.yTicks; i ++) {
+      for (let i = 0; i <= yTicks; i ++) {
         const h = this.cnv.height - (offset + yScale*ySpacing*i)
         this.ctx.moveTo(0,              h);
         this.ctx.lineTo(this.cnv.width, h);
@@ -227,7 +228,7 @@ class Plot extends HTMLElement {
 
       for (let i = 0; i < this.xTicks; i ++) {
         this.ctx.fillText(
-          `${Plot.monthDay(Math.floor(xSpacing*i))}`,
+          `${Plot.monthDay(Math.floor(xSpacing*i + xMin))}`,
           xScale*xSpacing*i,
           this.cnv.height - fontOffset
         );
@@ -445,30 +446,192 @@ class Plot extends HTMLElement {
 }
 customElements.define("plot-base", Plot);
 
-class PlotView extends HTMLElement {
+
+class PlotOutline extends Plot {
+
+  constructor(data) {
+    super(data);
+    // Override default behavior
+    this.axis = false;
+    this.fontHeight = 5;
+    this.aspectRatio = 0.12;
+    // Extend events
+    this.cnv.onmousedown = () => {this.onclick(true)};
+    this.cnv.onmouseup = () => {this.onclick(false)};
+    // Extend
+    this.boxColor = "#DDEAF3";
+    this.boxBackgroundColor = "RGBA(221,234,243,0.5)";
+    // Range status
+    this.rangeFrom = this.from;
+    this.rangeTo = this.to;
+    // Callbacks
+    this.onrangechange = () => {}; // Called when selected range changes
+  }
+
+  setRange(from, to) {
+    this.rangeFrom = from;
+    this.rangeTo = to;
+    this.show();
+    this.onrangechange();
+  }
+
+  /**
+  * show()
+  *
+  * Updates the render
+  * and does appropriate
+  * caching.
+  */
+  show() {
+    if (!this.restoreCache()) {
+      this.render(this.from, this.to, this.axis);
+      this.setCache();
+    }
+    this.renderBox(this.rangeFrom, this.rangeTo);
+  }
+
+  /**
+  * animate()
+  *
+  * Like show, but with an
+  * animation.
+  */
+  animate(step) {
+    step = step || 0.1;
+    let progress = 0;
+    let closure = () => {
+      if (progress < 1) {
+        this.render(this.from, this.to, this.axis, progress);
+        this.renderBox(this.rangeFrom, this.rangeTo);
+        progress += 0.1;
+        window.requestAnimationFrame(closure);
+      } else {
+        this.invalidateCache();
+        this.show();
+      }
+    };
+    window.requestAnimationFrame(closure);
+  }
+
+  onpointermove(event) {
+    if (!this.clicked) return;
+    // Determine highlight point
+    let hover = (event.clientX - this.cnv.getBoundingClientRect().left) / this.cnv.offsetWidth;
+    hover = Math.floor(hover * (this.to-this.from) + this.from);
+    // Determine if handle, drag, or none (and update)
+    const hitWidth = Math.ceil(this.x.length / 20);
+    if (Math.abs(hover-this.rangeFrom) < hitWidth) {
+      this.rangeFrom = Math.min(hover, this.rangeTo-hitWidth);
+    } else if (Math.abs(hover-this.rangeTo) < hitWidth) {
+      this.rangeTo = hover;
+    } else if (hover > this.rangeFrom && hover < this.rangeTo) {
+      const w = Math.floor((this.rangeTo - this.rangeFrom) / 2);
+      this.rangeFrom = Math.max(0, hover - w);
+      this.rangeTo = Math.min(this.x.length-1, hover + w);
+    } else {
+      return;
+    }
+    this.show();
+    this.onrangechange();
+  }
+
+  onclick(clicked) {
+    this.clicked = !!clicked;
+  }
+
+  /**
+  * renderBox()
+  *
+  * Highlight box over
+  * from -> to range, as
+  * opposed to previously
+  * rendered graph.
+  */
+  renderBox(from, to) {
+    // Restore values
+    const xMin     = this.__cacheXMin__;
+    const xMax     = this.__cacheXMax__;
+    const yMin     = this.__cacheYMin__;
+    const yMax     = this.__cacheYMax__;
+    const fromPrev = this.__cacheFrom__;
+    const toPrev   = this.__cacheTo__;
+
+    if (from < fromPrev || to > toPrev) throw "Index error";
+
+    const xScale = (this.cnv.width) / (xMax - xMin);
+    const handleWidth = 6 * Plot.pixelRatio;
+    const borderWidth = 2 * Plot.pixelRatio;
+
+    // Render background
+    this.ctx.fillStyle = this.boxBackgroundColor;
+    this.ctx.beginPath();
+    this.ctx.rect(0, 0, (this.x[from]-xMin)*xScale, this.cnv.height);
+    this.ctx.rect((this.x[to]-xMin)*xScale, 0, this.cnv.width, this.cnv.height);
+    this.ctx.fill();
+
+    // Render box
+    this.ctx.fillStyle = this.boxColor;
+    this.ctx.beginPath();
+    this.ctx.rect((this.x[from]-xMin)*xScale - handleWidth, 0, handleWidth, this.cnv.height);
+    this.ctx.rect((this.x[to]-xMin)*xScale, 0, handleWidth, this.cnv.height);
+    this.ctx.rect((this.x[from]-xMin)*xScale, 0, (this.x[to]-this.x[from])*xScale, borderWidth);
+    this.ctx.rect((this.x[from]-xMin)*xScale, this.cnv.height-borderWidth, (this.x[to]-this.x[from])*xScale, borderWidth);
+    this.ctx.fill();
+  }
+
+}
+customElements.define("plot-outline", PlotOutline);
+
+
+class PlotApp extends HTMLElement {
 
   constructor() {
     super();
     // Loading state (initial)
-    this.innerHTML = "<h1 class='loading'>Loading ...</h1>";
+    // TODO
     // Events
     this.onready = () => {};
     // Load data
-    PlotView.loadJson(this.json, charts => {
+    PlotApp.loadJson(this.dataset.json, charts => {
       // Plots
-      this.plotBig = new Plot(charts[0], this.canvBig);
+      this.heroPlot = new Plot(charts[+this.dataset.chart]);
+      this.scrollPlot = new PlotOutline(charts[+this.dataset.chart]);
+      this.scrollPlot.onrangechange = () => {
+        this.heroPlot.setFromTo(this.scrollPlot.rangeFrom, this.scrollPlot.rangeTo);
+      };
+      // Set nice starting range
+      this.scrollPlot.setRange(
+        Math.max(0, this.scrollPlot.x.length-41),
+        Math.min(this.scrollPlot.x.length, this.scrollPlot.x.length-11)
+      );
       // Setup html
-      this.innerHTML = "";
-      this.appendChild(this.plotBig);
-      // FIXME
-      setTimeout(() => {this.plotBig.show()}, 5);
+      this.appendChild(this.heroPlot);
+      this.appendChild(this.scrollPlot);
+      // Set up buttons
+      this.hiddenCount = 0;
+      this.heroPlot.names.forEach((name, i) => {
+        let button = document.createElement("a");
+        button.innerHTML = name;
+        button.setAttribute("data-color", this.heroPlot.colors[i]);
+        button.classList.add("checked");
+        button.onclick = () => {
+          if (button.classList.contains("checked") && this.hiddenCount > this.heroPlot.y.length - 2) {
+            // At least one plot has to be visible
+            return;
+          }
+          button.classList.toggle("checked");
+          this.heroPlot.togglePlot(i, true);
+          this.scrollPlot.togglePlot(i, true);
+          this.hiddenCount += button.classList.contains("checked") ? -1 : +1;
+        }
+        this.appendChild(button);
+      });
+      // Send ready event
       this.onready();
     });
-    console.log(this.style["test-prop"]);
   }
 
   static loadJson(path, callback) {
-    // FIXME
     // let req = new XMLHttpRequest();
     // req.loadend = data => {
     //   data = JSON.parse(data);
@@ -485,4 +648,4 @@ class PlotView extends HTMLElement {
   }
 
 }
-customElements.define("plot-view", PlotView);
+customElements.define("plot-app", PlotApp);
